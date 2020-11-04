b3d9e0badf52f1599e87cfe3078950b63d8f286e
whage <whage@freemail.hu>
2020-11-04 16:08:49 +0100

    69: trying channels, still very slow

--- problem_69/euler69.go
+++ problem_69/euler69.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+type ResultPair struct {
+	max int
+	maxRatio float64
+}
+
 // expects x > y
 func areRelativePrimes(x, y int) bool {
 	m := x % y
@@ -24,17 +29,58 @@ func totient(n int) int {
 	return count
 }
 
-func main() {
+func findMaxRatioOfRange(from, to int) (float64, int) {
 	var maxRatio float64 = 0
 	max := 2
-	for i := 2; i <= 20000; i++ {
+	for i := from; i <= to; i+=1 {
 		t := totient(i)
 		var ratio float64 = float64(i) / float64(t)
 		if ratio >= maxRatio {
 			maxRatio = ratio
 			max = i
 		}
-		//fmt.Printf("i: %d, totient(i): %d\n", i, totient(i))
+		fmt.Printf("i: %d, totient(i): %d\n", i, totient(i))
 	}
+	return maxRatio, max
+}
+
+func runInParallel() {
+	c := make(chan int)
+	go func() {
+		_, m:= findMaxRatioOfRange(2, 25000)
+		c <- m
+	} ()
+	go func() {
+		_, m:= findMaxRatioOfRange(25001, 50000)
+		c <- m
+	} ()
+	go func() {
+		_, m:= findMaxRatioOfRange(50001, 75000)
+		c <- m
+	} ()
+	go func() {
+		_, m:= findMaxRatioOfRange(75001, 100000)
+		c <- m
+	} ()
+
+	finishedCount := 0
+	max := 0
+	for result := range c {
+		finishedCount += 1
+		if finishedCount == 4 { close(c) }
+		if result >= max {
+			max = result
+		}
+	}
+	fmt.Printf("max: %d\n", max)
+}
+
+func runSequentially() {
+	maxRatio, max := findMaxRatioOfRange(2, 10000)
 	fmt.Printf("maxRatio: %f, max: %d\n", maxRatio, max)
+}
+
+func main() {
+	//runSequentially()
+	runInParallel()
 }
