e27312b73eb47fe5699964ee43936b923b43cc09
whage <ndrs.sallai@gmail.com>
2020-11-14 18:55:04 +0100

    63: get rid of ugly extra parameters

--- problem_63/euler63.rkt
+++ problem_63/euler63.rkt
@@ -5,12 +5,17 @@
 ;		  [acc 0])
 ;		(if (= base 9))))
 
-(define (get-count-for-constant-base base power acc)
-	(if (or (>= base 10) (< power 1))
-		acc
-		(if (>= (digit-count (expt base power)) power)
-			(get-count-for-constant-base base (+ 1 power) (+ 1 acc))
-			acc)))
+(define (get-count-for-constant-base base)
+	(let ([initial-power 1]
+		  [initial-acc 0])
+		(define (iterate-power power acc)
+			(if (>= base 10)
+				acc
+				(if (>= (digit-count (expt base power)) power)
+					(iterate-power (+ 1 power) (+ 1 acc))
+					acc)))
+		(iterate-power initial-power initial-acc)))
+	
 
 (define (get-count-for-constant-power base power acc)
 	(if (or (>= base 10) (< power 1))
@@ -29,5 +34,5 @@
 
 ;(get-count-for-constant-power 1)
 
-(get-count-for-constant-base 9 1 0)
+(get-count-for-constant-base 9)
 (get-count-for-constant-power 1 1 0)
