84d6717c3241c7d18b59b5f11b1fee2f646c6211
whage <ndrs.sallai@gmail.com>
2020-11-19 23:43:51 +0100

    61: WIP

--- 61-wip/61-wip.rkt
+++ 61-wip/61-wip.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require racket/trace)
+
 (define (triangle n)
     (/ (+ n (* n n)) 2))
 
@@ -33,9 +35,37 @@
 (define (four-digit-only numbers)
     (filter (lambda (i) (> i 999)) numbers))
 
-(four-digit-only (gen triangle))
-(four-digit-only (gen square))
-(four-digit-only (gen pentagonal))
-(four-digit-only (gen hexagonal))
-(four-digit-only (gen heptagonal))
-(four-digit-only (gen octagonal))
+(define (all-sets)
+    (list
+        (four-digit-only (gen triangle))
+        (four-digit-only (gen square))
+        (four-digit-only (gen pentagonal))
+        (four-digit-only (gen hexagonal))
+        (four-digit-only (gen heptagonal))
+        (four-digit-only (gen octagonal))))
+
+(all-sets)
+
+(define (filter-options node-idx options selected-values)
+    ;(let ([constraint-fn (list-ref constraints node-idx)])
+    (let ([constraint-fn constraint-0])
+        (filter (constraint-fn selected-values) options)))
+
+(define constraint-0
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            #t)))
+
+(define constraints
+    (list
+        constraint-0))
+
+(define (solve selected-values current-node-idx remaining-sets)
+    (if (= current-node-idx -1)
+        #f
+        (let ([options (filter-options current-node-idx remaining-sets selected-values)])
+            (if (empty? options)
+                (solve (list-set selected-values current-node-idx 0) (- current-node-idx 1) (get-remaining (take selected-values (max 0 (- current-node-idx 1)))))
+                (if (= current-node-idx 9)
+                    (list-set selected-values current-node-idx (car options))
+                    (solve (list-set selected-values current-node-idx (car options)) (+ 1 current-node-idx) (remove (car options) remaining-sets)))))))
