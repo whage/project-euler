416b3d12428b79aca5bb66dadec235a7300c2159
whage <ndrs.sallai@gmail.com>
2020-11-19 14:59:21 +0100

    68: stops at first solution

--- 68/68.rkt
+++ 68/68.rkt
@@ -3,59 +3,61 @@
 (require racket/trace)
 
 (define constraint-0
-	(lambda (selected-values) ; a function
-		(lambda (candidate) ; that returns a filtering function
-			(> candidate (list-ref selected-values 0)))))
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            (> candidate (list-ref selected-values 0)))))
 
 (define constraint-1
-	(lambda (selected-values) ; a function
-		(lambda (candidate) ; that returns a filtering function
-			(> candidate (list-ref selected-values 1)))))
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            (> candidate (list-ref selected-values 1)))))
 
 (define constraint-2
-	(lambda (selected-values) ; a function
-		(lambda (candidate) ; that returns a filtering function
-			(> candidate (list-ref selected-values 2)))))
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            (> candidate (list-ref selected-values 2)))))
 
 (define constraint-3
-	(lambda (selected-values) ; a function
-		(lambda (candidate) ; that returns a filtering function
-			(let ([sum-of-first-3 (apply + (take selected-values 3))]
-				  [sum-with-candidate (+ candidate (third selected-values))])
-				(and (> sum-of-first-3 sum-with-candidate) (> candidate (list-ref selected-values 3)))))))
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            (let ([sum-of-first-3 (apply + (take selected-values 3))]
+                  [sum-with-candidate (+ candidate (third selected-values))])
+                (and (> sum-of-first-3 sum-with-candidate) (> candidate (list-ref selected-values 3)))))))
 
 (define constraint-4
-	(lambda (selected-values) ; a function
-		(lambda (candidate) ; that returns a filtering function
-			(let ([sum-of-first-3 (apply + (take selected-values 3))]
-				  [sum-with-candidate (+ candidate (third selected-values) (fourth selected-values))])
-				(and (= sum-of-first-3 sum-with-candidate) (> candidate (list-ref selected-values 4)))))))
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            (let ([sum-of-first-3 (apply + (take selected-values 3))]
+                  [sum-with-candidate (+ candidate (third selected-values) (fourth selected-values))])
+                (and (= sum-of-first-3 sum-with-candidate) (> candidate (list-ref selected-values 4)))))))
 
 (define constraint-5
-	(lambda (selected-values) ; a function
-		(lambda (candidate) ; that returns a filtering function
-			(> candidate (list-ref selected-values 5)))))
+    (lambda (selected-values) ; a function
+        (lambda (candidate) ; that returns a filtering function
+            (let ([sum-of-first-3 (apply + (take selected-values 3))]
+                  [sum-with-candidate (+ candidate (second selected-values) (fifth selected-values))])
+                (and (= sum-of-first-3 sum-with-candidate) (> candidate (list-ref selected-values 5)))))))
 
 (define constraints (list constraint-0 constraint-1 constraint-2 constraint-3 constraint-4 constraint-5))
 
 (define (get-remaining list-b)
-	(let ([base (list 1 2 3 4 5 6)])
-		(sort (set->list (set-subtract (list->set base) (list->set list-b))) <)))
-	
+    (let ([base (list 1 2 3 4 5 6)])
+        (sort (set->list (set-subtract (list->set base) (list->set list-b))) <)))
+    
 
 (define (filter-options node-idx options selected-values)
-	(let ([constraint-fn (list-ref constraints node-idx)])
-		(filter (constraint-fn selected-values) options)))
+    (let ([constraint-fn (list-ref constraints node-idx)])
+        (filter (constraint-fn selected-values) options)))
 
 (define (solve selected-values current-node-idx remaining-numbers)
-	(if (= current-node-idx -1)
-		#f
-		(if (= current-node-idx 5)
-			(list-set selected-values current-node-idx (list-ref remaining-numbers 0))
-			(let ([options (filter-options current-node-idx remaining-numbers selected-values)])
-				(if (not (empty? options))
-					(solve (list-set selected-values current-node-idx (car options)) (+ 1 current-node-idx) (remove (car options) remaining-numbers))
-					(solve (list-set selected-values current-node-idx 0) (- current-node-idx 1) (get-remaining (take selected-values (- current-node-idx 1)))))))))
+    (if (= current-node-idx -1)
+        #f
+        (let ([options (filter-options current-node-idx remaining-numbers selected-values)])
+            (if (empty? options)
+                (solve (list-set selected-values current-node-idx 0) (- current-node-idx 1) (get-remaining (take selected-values (- current-node-idx 1))))
+                (if (= current-node-idx 5)
+                    (list-set selected-values current-node-idx (car options))
+                    (solve (list-set selected-values current-node-idx (car options)) (+ 1 current-node-idx) (remove (car options) remaining-numbers)))))))
 
 (trace get-remaining)
 (trace filter-options)
