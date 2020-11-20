06712aa67ac8707c1e0e4f0f4645cf8f74919b81
whage <ndrs.sallai@gmail.com>
2020-11-20 22:04:09 +0100

    61: WIP, backtracking is still broken

--- 61-wip/61-wip.rkt
+++ 61-wip/61-wip.rkt
@@ -20,17 +20,16 @@
 (define (octagonal n)
     (* n (- (* 3 n) 2)))
 
-(define (generate-numbers generator n upto numbers)
+(define (generate-numbers generator n upto)
     (let ([current (generator n)])
         (if (> current upto)
-            numbers
-            (generate-numbers generator (+ n 1) upto (cons current numbers)))))
+            (list)
+            (append (list current) (generate-numbers generator (+ n 1) upto)))))
 
 (define (gen generator)
     (let ([start 1]
-          [upto 9999]
-          [results (list)])
-        (generate-numbers generator start upto results)))
+          [upto 9999])
+        (generate-numbers generator start upto)))
 
 (define (four-digit-only numbers)
     (filter (lambda (i) (> i 999)) numbers))
@@ -51,10 +50,14 @@
 (four-digit-only (gen heptagonal))
 (four-digit-only (gen octagonal))
 
-(define (get-available-options current-value options)
-    (if (= current-value 0)
-        options
-        (filter (lambda (n) (= (quotient n 100) (modulo current-value 100))) options)))
+(define (get-available-options selected-values current-node-idx options)
+    (let ([left-number (list-ref selected-values (max 0 (- current-node-idx 1)))]
+          [current-number (list-ref selected-values current-node-idx)])
+        (if (= current-node-idx 0)
+            (filter (lambda (n) (> n current-number)) options)
+            (if (= current-node-idx 5)
+                (filter (lambda (n) (and (> n current-number) (= (quotient n 100) (modulo left-number 100)) (= (quotient (first selected-values) 100) (modulo n 100)))) options)
+                (filter (lambda (n) (and (> n current-number) (= (quotient n 100) (modulo left-number 100)))) options)))))
 
 (define (get-available-sets used-sets unusable-sets)
     (sort (set->list (set-subtract (list->set (list 0 1 2 3 4 5)) (list->set used-sets) (list->set unusable-sets))) <))
@@ -66,14 +69,13 @@
             (if (empty? indexes-of-available-sets)
                 (solve (list-set selected-values current-node-idx 0) (- current-node-idx 1) (rest used-sets) (list)) ; backtrack
                 (let* ([first-available-set (list-ref (all-sets) (first indexes-of-available-sets))]
-                       [options (get-available-options (list-ref selected-values (max 0 (- current-node-idx 1))) first-available-set)])
+                       [options (get-available-options selected-values current-node-idx first-available-set)])
                     (if (empty? options)
                         (solve selected-values current-node-idx used-sets (cons (first indexes-of-available-sets) unusable-sets)) ; try next set for current node
                         (if (= current-node-idx 5)
                             (list-set selected-values current-node-idx (first options))
-                            (solve (list-set selected-values current-node-idx (first options)) (+ 1 current-node-idx) (cons (first indexes-of-available-sets) used-sets) (list))))))))) ; pick and move on to next node
+                            (solve (list-set selected-values current-node-idx (first options)) (+ 1 current-node-idx) (cons (first indexes-of-available-sets) used-sets) (list))))))))) ; pick first move on to next node
 
 (trace solve)
 (trace get-available-sets)
-;(trace get-available-options)
 (solve (list 0 0 0 0 0 0) 0 (list) (list))
