2ca5eeab9080254f3e2dbc0bc70ca606286e64d5
whage <ndrs.sallai@gmail.com>
2020-11-21 12:01:28 +0100

    70: add comments

--- 70/70.rkt
+++ 70/70.rkt
@@ -2,22 +2,27 @@
 
 (require math/number-theory)
 
+; returns the digits of given number as a list
 (define (get-digits number acc)
     (if (= (quotient number 10) 0)
         (cons (modulo number 10) acc)
         (get-digits (quotient number 10) (cons (modulo number 10) acc))))
 
+; sorts the digits of given number
 (define (get-sorted-digits number)
     (let ([acc (list)])
         (sort (get-digits number acc) <)))
 
+; a brute force algorithm for finding totient of n
+; very inefficient but runs correctly
 (define (brute-force-totient n other acc)
         (if (= other 0)
             acc
             (if (coprime? n other)
-                (totient n (- other 1) (+ 1 acc))
-                (totient n (- other 1) acc))))
+                (totient n (- other 1) (+ 1 acc)) ; if the two numbers are coprime then add 1 to acc and move forward
+                (totient n (- other 1) acc)))) ; if not, then just move forward without incrementing acc
 
+; wrapper fucntion for brute force totient algorithm
 (define (totient-using-brute-force n)
     (if (= n 1)
         n
@@ -39,6 +44,7 @@
 ;           )
 ;       (totient-using-prime-factorization n (+ i 1) (- result (/ result i)))))
 
+; checks every number from i -> limit while keeping track of the min-ratio
 (define (iterate i limit min-ratio min-so-far)
     (if (> i limit)
         min-so-far
@@ -49,8 +55,10 @@
                     min-so-far))
                 (iterate (+ i 1) limit min-ratio min-so-far)))))
 
+; wrapper function
 (define (find-min)
     (iterate 2 10000000 10 2)
     )
 
+; runs for a very long time!
 (find-min)
